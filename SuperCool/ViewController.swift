4fce175eb07f1be14b00f3e0ed6acda2eb08d95f
Jason McCoy <jasonmccoy@Jasons-MacBook-Pro.local>
2016-04-04 14:41:23 -0700

    I Love Irene App
    Hope we don’t break up.

--- SuperCool/ViewController.swift
+++ SuperCool/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coolLogo: UIImageView! // I Love Irene
+    @IBOutlet weak var myBackground: UIImageView! // Hearts on the Left side
+    @IBOutlet weak var awesomeButton: UIButton! // "Irene's Special Button"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func awesomeButtonAction(sender: AnyObject) {
+        coolLogo.hidden = false
+        myBackground.hidden = false
+        awesomeButton.hidden = true
+    }
 
 }
 
